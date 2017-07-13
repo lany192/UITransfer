dad13456ddbc7459a43a4ad64de361ba983b13cf
Lany <lany192@qq.com>
2017-07-13 13:58:15 +0800

    update

---
+++ app/src/main/java/com/lany/uitransfer/MyApp.java
@@ -0,0 +1,14 @@
+package com.lany.uitransfer;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/7/13 0013.
+ */
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
